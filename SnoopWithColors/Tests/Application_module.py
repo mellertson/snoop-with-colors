ad393f7506ed751a9a263cfd626fcc190c2b5335
Mike Ellertson <mdellertson@gmail.com>
2017-03-11 23:21:29 -0800

    Completed test cases for parseArgs() method.

--- SnoopWithColors/Tests/Application_module.py
+++ SnoopWithColors/Tests/Application_module.py
@@ -36,7 +36,29 @@ class TestCase_parseArgs(unittest.TestCase):
 		sys.argv.append(33)
 		with self.assertRaises(RuntimeError):
 			self.app.parseArgs()
-
-
+			
+		# restore the original command line arguments
+		sys.argv=argv
+	def test_return_value(self):
+		"""
+		Test the return value of SnoopWithColors.Application.parseArgs()
+		"""
+		argv = sys.argv
+		
+		# test parse args with no command line arguments
+		sys.argv = argv[0:1]
+		sys.argv.append(31)
+		sys.argv.append(32)
+		args = self.app.parseArgs()
+		
+		# verify the number of args returned
+		self.assertEqual(len(args), 2, "Expected 2 command line args, but got {}".format(len(args)))
+		
+		# verify the values of the returned args
+		self.assertEqual(args[0], 31)
+		self.assertEqual(args[1], 32)
+		
+		# restore the original command line arguments
+		sys.argv=argv
 if __name__ == '__main__':
 	unittest.main()
